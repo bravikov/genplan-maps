93d55139b5ec10a458613e57d28011a050e11acb
Dmitry Bravikov <dmitry@bravikov.pro>
2024-10-26 23:07:14 +0300

    Добавил robots.txt

--- system/urls.py
+++ system/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 import genplan.views
 
 urlpatterns = [
     path('', genplan.views.index_page),
     path('about', genplan.views.about_page),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
     path('<str:city_name>', genplan.views.city_page),
     path('<str:city_name>/<str:city_map_name>', genplan.views.city_map_page),
 ]
